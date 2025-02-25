61923912a1f714e4ac47e67206e87737b768935f
Ivan <0675361905vi@gmail.com>
2025-02-25 17:30:18 +0200

    feat: add markup and styles for the first page

---
+++ src/components/IButton/IButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="bg-[#FFA279] rounded-xl px-10 py-3.5 text-white font-bold tracking-wider-[-0.408px] text-[16px] leading-[22px]"
+  >
+    Почати роботу
+  </button>
+</template>
